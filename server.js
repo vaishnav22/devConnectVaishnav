e5614619d02988a81c26a51a31eecde2c0edae9c
vaishnav22 <vaishnavslvp@gmail.com>
2020-11-19 13:18:17 +0530

    Prepared for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
+const path = require('path')
 
 
 const users = require('./routes/api/users')
@@ -32,6 +33,14 @@ app.use('/api/users', users)
 app.use('/api/profile', profile)
 app.use('/api/posts', posts)
 
+//if sever dosent open look for buil/static/index
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+
+    app.get('*', (req,res)=> {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000;
 
