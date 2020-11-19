cbc5635f434f21b50e5db4d481f478d721a9b000
vaishnav22 <vaishnavslvp@gmail.com>
2020-11-19 15:35:18 +0530

    Prepared for deployment

--- client/src/store.js
+++ client/src/store.js
@@ -6,6 +6,6 @@ const initialState = {}
 
 const middleware = [thunk]
 
-const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware),typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined" ? a=>a : window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
 export default store;
